ba35a9484f3ab010d3195343ab99578fa098c9a0
oliviaworks <seuemail@exemplo.com>
2024-10-28 09:57:01 -0300

    a

--- example.py
+++ example.py
@@ -3,20 +3,17 @@ from flask_api import FlaskAPI, status, exceptions
 
 app = FlaskAPI(__name__)
 
-
 notes = {
     0: "Germinare Tech - Confie no seu esforço e na sua preparação: cada desafio em DevOps é uma chance de mostrar o que você sabe e o quanto evoluiu. Vá com calma e com determinação, você consegue!",
-    1: "Pipeline é VIDA - Confie no seu esforço e na sua preparação. Cada desafio é uma chance de mostrar o que você sabe e o quanto cresceu. Vá com calma, você consegue! ",
+    1: "Pipeline é VIDA - Confie no seu esforço e na sua preparação. Cada desafio é uma chance de mostrar o que você sabe e o quanto cresceu. Vá com calma, você consegue!",
     2: "Cada passo na AWS Cloud é um avanço em direção à inovação e à eficiência. Confie no seu aprendizado e explore sem limites; o céu é o ponto de partida!"
 }
-
 
 def note_repr(key):
     return {
         "url": request.host_url.rstrip("/") + url_for("notes_detail", key=key),
         "text": notes[key],
     }
-
 
 @app.route("/", methods=["GET", "POST"])
 def notes_list():
@@ -31,7 +28,6 @@ def notes_list():
 
     # request.method == 'GET'
     return [note_repr(idx) for idx in sorted(notes.keys())]
-
 
 @app.route("/<int:key>/", methods=["GET", "PUT", "DELETE"])
 def notes_detail(key):
@@ -52,6 +48,5 @@ def notes_detail(key):
         raise exceptions.NotFound()
     return note_repr(key)
 
-
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(port=5000, debug=False)
